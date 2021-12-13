ecb21f75a2a7b3a7f686138b65fdd11a90e5a46a
Joshua <joshuaegage@gmail.com>
2021-12-12 19:13:14 -0600

    new commit
    added base files and personal site files

---
+++ base/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang='en'>
+
+	<head>
+		<title>Website</title>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<meta name='description' description='[[ insert description ]]'>
+		<meta property='og:image' content='images/default-meta.png'>
+		<link rel='stylesheet' href='css/style.css'>
+	</head>
+
+	<body>
+		
+			<header>
+				<div class="inner-column">
+					<h1>Default Header</h1>
+					<nav>Navigation</nav>
+				</div>
+			</header>
+
+			<main>
+
+				<!-- main content -->
---
+++ personal-site/footer.php
@@ -0,0 +1,11 @@
+
+<!-- main content -->
+</main>
+
+		<footer>
+			<div class="inner-column">Contact me</div>
+		</footer>
+
+	</body>
+
+</html>
