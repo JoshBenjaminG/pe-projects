9639835d363bd8b3b7aa07de95ba60d6a9601bd7
Joshua <joshuaegage@gmail.com>
2022-01-17 19:39:34 -0600

    Big ole' commit

--- super-layout-practice/modules/graphic-diptych.php
+++ super-layout-practice/modules/graphic-diptych.php
@@ -1,16 +1,14 @@
 
 <graphic-diptych>
 
-
+	<picture>
+		<img src='https://peprojects.dev/images/square.jpg' alt='$todo'>
+	</picture>
 
 	<text-content>
 		<h1 class='intro-voice'>Hello! This is a 'diptych'</h1>
 
 		<p class='calm-voice'>You can call it whatever you want. It's a module that has an image and some supporting text. On the smaller screen it works nicely to stack. On a larger screen, it fits side by side.</p>
 	</text-content>
-
-	<picture>
-		<img src='https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1768&q=80' alt='$todo'>
-	</picture>
 
 </graphic-diptych>
