11b319668cfea0ccbcdd2082c28e8676334bb56c
Joshua <joshuaegage@gmail.com>
2022-01-30 00:12:12 -0600

    more practice

---
+++ php-practice/loops2.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$array = [
+	"hair" => "curly",
+	"name" => "josh",
+	"age" => "25",
+	"occupation" => "student",
+	"phrase" => "ayo",
+];
+
+$message = "<p>" . $array['name'] . " is " . $array['age'] . " and is a " . $array['occupation'] . " He has " . $array['hair'] . " hair, and likes to say " . $array['phrase'] . " </p>";
+
+echo $message;
+
+
+?>
