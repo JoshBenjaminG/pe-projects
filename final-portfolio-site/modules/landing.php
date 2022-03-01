e086a856112733aaec043d2f7260f10e42b31dc2
Joshua <joshuaegage@gmail.com>
2022-02-28 23:11:35 -0600

    more

--- final-portfolio-site/modules/landing.php
+++ final-portfolio-site/modules/landing.php
@@ -1,6 +1,7 @@
+<div class="blue">blue</div>
+
 <div class="inner-column">
 	<ul>
-
 		<div class="inner-column">
 			<h1 class="intro-voice">Joshua Gage</h1>
 			<p class="calm-voice">Your front-end web developer</p>
