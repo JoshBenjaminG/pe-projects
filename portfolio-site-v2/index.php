cbfd7966727cf56810453e4ed9f5f37268a2398d
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2022-05-15 04:36:35 -0500

    Next iteration of portfolio site

---
+++ portfolio-site-v2/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel='stylesheet' href='css/style.css'>
+</head>
+<body>
+
+<section>
+	<header>
+		<nav>
+			<a href="">Projects</a>
+			<a href="">Resume</a>
+			<a href="">Contact</a>
+		</nav>
+	</header>
+</section>
+
+<section>
+	<h1>Hello, I'm Josh!</h1>
+	<p>Having taken an interest in programming in my last year of school, I enrolled in and completed a remote program while finishing my degree.</p>
+</section>
+
+<section>
+	<h1>My Projects</h1>
+
+	<ul>
+		<li>
+			<h2>Project 1</h2>
+			<p>This is a project example</p>
+			<a href="">Live Demo</a>
+		</li>
+		<li>
+			<h2>Project 1</h2>
+			<p>This is a project example</p>
+			<a href="">Live Demo</a>
+		</li>
+	</ul>
+</section>
+
+<section>
+	<footer>
+		<p>Josh Gage</p>
+		<a href="">LinkedIn</a>
+		<a href="">Codepen</a>
+		<a href="">Github</a>
+	</footer>
+</section>
+
+</body>
+</html>
