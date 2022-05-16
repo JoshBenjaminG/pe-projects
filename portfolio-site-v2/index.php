8e05334e3999d8a360397913dc524ad3a8dc413a
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2022-05-16 07:46:49 -0500

    Gradient Background test

--- portfolio-site-v2/index.php
+++ portfolio-site-v2/index.php
@@ -5,49 +5,98 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 	<link rel='stylesheet' href='css/style.css'>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
 
-<section>
-	<header>
-		<nav>
-			<a href="">Projects</a>
-			<a href="">Resume</a>
-			<a href="">Contact</a>
-		</nav>
-	</header>
+<main>
+
+
+<div class="header">
+	<div class="inner-column">
+		<header>
+			<nav>
+				<span>
+					<a href="" class="logo">JG</a>
+				</span>
+				<span>
+					<a href="" class="hover-underline-animation">Projects</a>
+					<a href="" class="hover-underline-animation">Resume</a>
+					<a href="" class="hover-underline-animation">Contact</a>
+				</span>
+			</nav>
+		</header>
+	</div>
+</div>
+
+
+<section class="landing">
+	<div class="inner-column">
+		<p>Greetings,</p>
+		<h1 class="hover-h1-animation">I'm Josh!</h1>
+		<div class="landing-p-right">
+			<p>- Web Developer</p>
+		</div>
+	</div>
+
 </section>
 
-<section>
-	<h1>Hello, I'm Josh!</h1>
-	<p>Having taken an interest in programming in my last year of school, I enrolled in and completed a remote program while finishing my degree.</p>
+<!-- <section class="about-heading">
+	<div class="inner-column">
+		<h2 class="hover-about-animation">About me</h2>
+	</div>
+</section> -->
+
+<section class="about">
+	<div class="inner-column">
+		<p class="p-left">A web developer from the Gulf Coast, I discovered my passion for development in my last year of college. While I completed my formal education, I enrolled in a remote web development program to build on and reinforce my learning progress.</p>
+		<p class="p-right">Below you will find a few projects of mine. A capstone project that combines everything I have learned so far, as well as other projects meant to cement my skills in a focused manner. Don't hesitate to contact me!</p>
+	</div>
 </section>
 
-<section>
-	<h1>My Projects</h1>
-
-	<ul>
-		<li>
-			<h2>Project 1</h2>
-			<p>This is a project example</p>
-			<a href="">Live Demo</a>
-		</li>
-		<li>
-			<h2>Project 1</h2>
-			<p>This is a project example</p>
-			<a href="">Live Demo</a>
-		</li>
-	</ul>
+<section class="projects">
+	<div class="inner-column">
+		<h2>My Projects</h1>
+		<p>Below are a list of a few projects that I have made.</p>
+		<div class="project-list">
+			<ul>
+				<li>
+					<h3>Project 1</h2>
+					<p>This is a project example</p>
+					<a href="">Live Demo</a>
+				</li>
+				<li>
+					<h3>Project 1</h2>
+					<p>This is a project example</p>
+					<a href="">Live Demo</a>
+				</li>
+				<li>
+					<h3>Project 1</h2>
+					<p>This is a project example</p>
+					<a href="">Live Demo</a>
+				</li>
+			</ul>
+		</div>
+		<a href="">View more</a>
+	</div>
 </section>
 
-<section>
-	<footer>
-		<p>Josh Gage</p>
-		<a href="">LinkedIn</a>
-		<a href="">Codepen</a>
-		<a href="">Github</a>
-	</footer>
-</section>
+</main>
+
+<!-- <div>	
+	<div class="inner-column">
+		<footer>
+			<p>Josh Gage</p>
+			<a href="">LinkedIn</a>
+			<a href="">Codepen</a>
+			<a href="">Github</a>
+		</footer>	
+	</div>
+</div> -->
+
+
 
 </body>
 </html>
