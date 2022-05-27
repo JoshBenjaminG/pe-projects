44ea781341b94cf3cf5645431aca891ab2669419
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2022-05-27 00:09:30 -0500

    Added images to monsters

--- php-practice/gpt-3.php
+++ php-practice/gpt-3.php
@@ -1,14 +1,42 @@
 <?php 
 
-$words = [
-    "noun" => ["he", "she"],
-    "verb" => ["run", "swim"],
-    "adverb" => ["quickly", "slowly"],
-];
+    include("style.php");
 
-foreach($words["noun"] as $word) {
-    echo $word;
-}
+    $lima = [
+        "img" => "limabean.jpg",
+        "name" => "lima",
+        "food" => "beans",
+        "age" => 4,
+    ];
+
+    $cody = [
+        "img" => "codey.jpg",
+        "name" => "cody",
+        "food" => "cake",
+        "age" => 7,
+    ];
+
+    $reads = [
+        "img" => "miss-reads-a-lot.jpg",
+        "name" => "reads",
+        "food" => "pizza",
+        "age" => 6,
+    ];
+
+    $monsters = [$lima, $cody, $reads];
+
+    foreach($monsters as $monster) {
+        echo "<monster-card>" .
+
+                "<img src='" . $monster["img"] . "'></img>" .
+
+                "<p>Hello, I'm " . $monster["name"] . "! </p>" .
+
+                "<p>My favorite food is " . $monster["food"] . "! </p>" .
+
+                "<p>I am " . $monster["age"] . " years old.</p>" .
+
+            "</monster-card>";
+    }
 
 ?>
-
--- php-practice/style.php
+++ php-practice/style.php
@@ -25,4 +25,18 @@ label {
 	margin-bottom: 10px;
 	margin-top: 10px;
 }
+
+monster-card {
+	background-color: red;
+	margin: 20px;
+	padding: 20px;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	align-items: center;
+}
+
+img {
+	width: 150px;
+}
 </style>
