b9f4c1c63e57e563199c3f290b4616745bbf9f76
Joshua <joshuaegage@gmail.com>
2022-06-18 16:53:23 -0500

    Add Photography Theme

--- projects/riche-fashion/modules/head.php
+++ projects/riche-fashion/modules/head.php
@@ -7,7 +7,7 @@
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
 
 		<title>RICHE US Official Site | Since 1989</title>
-		<meta name='description' content='PSSST (CSS)'>
+		<meta name='description' content='RICHE US Official Site | Since 1989'>
 		<meta property='og:image' content='A CSS methodology so great, it needed a name.'>
 
 		<link rel='stylesheet' href='styles/site.css'>
