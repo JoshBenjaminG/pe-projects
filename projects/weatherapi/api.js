a7eeb0ddd16af978865fd689079abd73f565b5c5
Joshua <joshuaegage@gmail.com>
2022-10-08 03:23:46 -0500

    added variable condition icons

--- projects/weatherapi/api.js
+++ projects/weatherapi/api.js
@@ -21,10 +21,16 @@ function createResults(result) {
 	return `
 		<h1>Weather in ${result.location.name}</h1>
 		<p id="temp">${result.current.temp_f}&#8457</p>
+		<div class='condition'>
+		<picture>
+			<img src="${result.current.condition.icon}" alt="">
+		</picture>
 		<p>Condition: ${result.current.condition.text}</p>
+		</div>
 		<p>Precipitation: ${result.current.precip_in} inches</p>
 		<p>Humidity: ${result.current.humidity}%</p>
 		<p>Visibility: ${result.current.vis_miles} miles</p>
+		<p>Image source: Unslpash.com</p>
 	`;
 }
 
