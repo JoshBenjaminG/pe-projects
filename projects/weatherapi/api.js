db2c596531ebf7d85bf7efc0cfdbe24d5701ff7a
Joshua <joshuaegage@gmail.com>
2022-10-08 21:27:50 -0500

    added forecast logic and styles

--- projects/weatherapi/api.js
+++ projects/weatherapi/api.js
@@ -4,6 +4,7 @@ var city = "";
 
 
 const output = document.querySelector('output');
+const output2 = document.querySelector('daysoutput');
 
 const form = document.querySelector('form');
 
@@ -20,7 +21,7 @@ function heatWarning(heat) {
 function createResults(result) {
 	return `
 		<h1>Weather in ${result.location.name}</h1>
-		<p id="temp">${result.current.temp_f}&#8457</p>
+		<p class="temp">${result.current.temp_f}&#8457</p>
 		<div class='condition'>
 		<picture>
 			<img src="${result.current.condition.icon}" alt="">
@@ -34,6 +35,25 @@ function createResults(result) {
 	`;
 }
 
+function createForecast(result) {
+	result.forecast.forecastday.forEach( function(entry) {
+		output2.innerHTML += `
+			<div class="day">
+			<content>
+				<p>${entry.date}</p>
+				<p class="temp">${entry.day.avgtemp_f}&#8457</p>
+				<div class='condition'>
+				<picture>
+					<img src="${entry.day.condition.icon}" alt="">
+				</picture>
+				<p>Condition: ${entry.day.condition.text}</p>
+				</div>
+			</content>
+			</div>
+		`;
+	});
+}
+
 function createError(input) {
 	return `
 		<h1>There was an error: ${input} does not exist.</h1>
@@ -42,15 +62,22 @@ function createError(input) {
 
 function getApi() {
 	var city = input.value;
-	var request = "https://api.weatherapi.com/v1/current.json?key=1893971150fa4bdd969223007221509&q=" + city + "&aqi=no";
+	var request = "https://api.weatherapi.com/v1/forecast.json?key=1893971150fa4bdd969223007221509&q=" + city + "&days=5&aqi=no&alerts=no";
 	fetch(request)
 		.then( function (response) {
 			return response.json();
 		})
 		.then( function(json) {
 			output.innerHTML = createResults(json);
+			output2.innerHTML = "";
+			createForecast(json);
 			console.log(json);
-			document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + city + "')";
+			if (city == "mobile") {
+				document.body.style.backgroundImage = "url('https://www.cityofmobile.org/assets/images/mobile-hdr.jpg')";
+			} else {
+				document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + city + "')";
+			}
+			
 		})
 		.catch( function(error) {
 			output.innerHTML = createError(city);
