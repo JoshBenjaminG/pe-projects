d2c6752b2c2ad33b25e104fe7cf111eb188fde3f
Joshua <joshuaegage@gmail.com>
2022-10-19 09:26:40 -0500

    added album and artist images

--- projects/wordpress-josh/wp-content/themes/artists-2022/album-card.php
+++ projects/wordpress-josh/wp-content/themes/artists-2022/album-card.php
@@ -1,5 +1,8 @@
 <card>
 	<h2><?php echo the_title(); ?></h2>
-	<h2><?php echo the_field('release_date'); ?></h2>
-	<h2><?php echo the_field('length'); ?></h2>
+	<picture>
+		<img src="<?php echo the_field('album_image'); ?>" alt="">
+	</picture>
+	<h2>Release Date: <?php echo the_field('release_date'); ?></h2>
+	<h2>Album Length: <?php echo the_field('length'); ?></h2>
 </card>
--- projects/wordpress-josh/wp-content/themes/artists-2022/artist.php
+++ projects/wordpress-josh/wp-content/themes/artists-2022/artist.php
@@ -1,7 +1,10 @@
 
 <card>
 	<h2><?php echo the_field('name'); ?></h2>
-	<h2><?php echo the_field('age'); ?></h2>
+	<picture>
+		<img src="<?php echo the_field('image'); ?>" alt="">
+	</picture>
+	<h2>Years old: <?php echo the_field('age'); ?></h2>
 
 	<a href="<?php the_permalink(); ?>">Read more</a>
 </card>
