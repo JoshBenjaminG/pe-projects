2daf414266a04bf5d1ef979d43f3a433ff166988
Joshua <joshuaegage@gmail.com>
2022-10-21 21:03:12 -0500

    added acf repeater loop test

--- projects/wordpress-josh/wp-content/themes/artists-2022/templates/album-card.php
+++ projects/wordpress-josh/wp-content/themes/artists-2022/templates/album-card.php
@@ -5,4 +5,27 @@
 	</picture>
 	<h2>Release Date: <?php echo the_field('release_date'); ?></h2>
 	<h2>Album Length: <?php echo the_field('length'); ?></h2>
+<?php
+
+// Check rows existexists.
+if( have_rows('track') ):
+
+    // Loop through rows.
+    while( have_rows('track') ) : the_row();
+
+        // Load sub field value.
+        $sub_value = get_sub_field('title');
+        $sub_length = get_sub_field('length');
+        echo $sub_value . ": ";
+        echo $sub_length;
+        echo "<br>";
+
+    // End loop.
+    endwhile;
+
+// No value.
+else :
+    // Do something...
+endif;
+?>
 </card>
