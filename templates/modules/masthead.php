6820f7fb0e3d89eca6b227c11155d4cd6a94a00a
Joshua <joshuaegage@gmail.com>
2023-01-01 23:55:31 -0600

    Remove writing intro section

--- templates/modules/masthead.php
+++ templates/modules/masthead.php
@@ -18,19 +18,15 @@ if ($page == 'home') {
 	<nav class='site-menu'>
 		<a class='logo' href='?page=home'>
 			<img src="images/JG-logo.svg" alt="">
-			<!-- or you could actually use the SVG -->
 		</a>
 
 	</nav>
 
 
 	<nav class='links'>
-		<!-- <a href='?page=home' class="hover-underline-animation calm-voice">Home</a> -->
 		<a href="?page=home" class="hover-underline-animation">Home</a>
 		<a href='?page=style-guide' class="hover-underline-animation calm-voice projects-link">Style Guide</a>
 		<a href="https://github.com/JoshBenjaminG" target="_blank" class="hover-underline-animation calm-voice">Github</a>
-		<!-- <a href="https://resume.creddle.io/resume/j9rrzc7had4" class="hover-underline-animation calm-voice">Resume</a> -->
-	<!-- 	<a href='?page=goals' class="hover-underline-animation calm-voice">Goals</a> -->
 	</nav>
 
 </mast-head>
