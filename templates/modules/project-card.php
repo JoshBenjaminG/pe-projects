bee6ded67ef104477fcaefe54b057e28d6808f87
Joshua <joshuaegage@gmail.com>
2023-01-30 12:51:01 -0600

    add live demo links

--- templates/modules/project-card.php
+++ templates/modules/project-card.php
@@ -2,13 +2,17 @@
 	// you could simplify the $variables up here if you want....
 	// example
 	$thumbnail = $project["thumbnail"];
+	$url = $project["url"];
+	$caseStudy = $project["hasCaseStudy"];
+	$hasRepo = $project["hasRepo"];
+	$repo = $project["gitrepo"];
+	
 	// it may or may not - be more readableloud
 
 ?>
 
 
 <project-card>
-	<a href='?page=detail&project=<?=$project['slug']?>' class='project-link'>
 		<picture>
 			<img src='<?=$thumbnail?>' loading='lazy' alt='$todo'>
 		</picture>
@@ -18,5 +22,14 @@
 		<h3 class='calm-voice tech'><?=$project['technologies']?></h3>
 
 		<p class='calm-voice'><?=$project['description']?></p>
-	</a>
+
+		<div>
+		<?php if ($caseStudy == true) { ?>
+			<a href='?page=detail&project=<?=$project['slug']?>' target="_blank" class="calm-voice">Read Case Study</a>
+		<?php }?>
+		<?php if ($hasRepo == true) { ?>
+		<a href='<?=$repo?>' target="_blank" class="calm-voice">Github Repo</a>
+		<?php } ?>
+		<a href='<?=$url?>' target="_blank" class="calm-voice">Live Demo</a>
+		</div>
 </project-card>
