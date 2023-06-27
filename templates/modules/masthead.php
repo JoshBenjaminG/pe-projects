e78c3c1f25f3005d0283d880fc6574f604096433
Joshua <joshuaegage@gmail.com>
2023-06-27 01:09:46 -0500

    edit codepen link

--- templates/modules/masthead.php
+++ templates/modules/masthead.php
@@ -25,7 +25,7 @@ if ($page == 'home') {
 
 	<nav class='links'>
 		<a href="?page=home" class="hover-underline-animation calm-voice">Home</a>
-		<a href='?page=style-guide' class="hover-underline-animation calm-voice projects-link">Style Guide</a>
+		<a href="https://codepen.io/JoshuaEg/pens/public" target="_blank" class="hover-underline-animation calm-voice">Codepen</a>
 		<a href="https://github.com/JoshBenjaminG" target="_blank" class="hover-underline-animation calm-voice">Github</a>
 	</nav>
 
