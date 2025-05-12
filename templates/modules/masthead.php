45b533643e31612eec78c25696af6a756af733d4
Joshua <joshuaegage@gmail.com>
2025-05-12 16:38:17 -0500

    logo href

--- templates/modules/masthead.php
+++ templates/modules/masthead.php
@@ -16,7 +16,7 @@ if ($page == 'home') {
 <mast-head class='<?=$class?>'>
 
 	<nav class='site-menu'>
-		<a class='logo' href='?page=home'>
+		<a class='logo' href='https://josheg.com/'>
 			<img src="images/JG-logo.svg" alt="">
 		</a>
 
@@ -24,7 +24,7 @@ if ($page == 'home') {
 
 
 	<nav class='links'>
-		<a href="?page=home" class="hover-underline-animation calm-voice">Home</a>
+		<a href="https://josheg.com/" class="hover-underline-animation calm-voice">Home</a>
 		<a href="https://codepen.io/JoshuaEg/pens/public" target="_blank" class="hover-underline-animation calm-voice">Codepen</a>
 		<a href="https://github.com/JoshBenjaminG" target="_blank" class="hover-underline-animation calm-voice">Github</a>
 	</nav>
