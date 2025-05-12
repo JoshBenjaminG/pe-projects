f93e8f36e1b9cfcdb64d89bbd1ac9f12e1371438
Joshua <joshuaegage@gmail.com>
2025-05-12 15:50:23 -0500

    jot form

--- templates/modules/contact-me.php
+++ templates/modules/contact-me.php
@@ -1,47 +1,25 @@
-<?php
-$error_message = "";
-$message = false;
-
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $bodyMessage = $_POST['message'];
-
-    if (!empty($name) && !empty($bodyMessage)) {
-        // Set up email parameters
-        $to = 'joshuaegage@gmail.com';
-        $subject = 'Personal site message';
-        $messageBody = $name . " wrote:\n\n" . $bodyMessage . "\n\nThe email is: " . $email;
-        $headers = 'From: ' . $email . "\r\n" .
-                   'Reply-To: ' . $email . "\r\n" .
-                   'X-Mailer: PHP/' . phpversion();
-
-        // Send email
-        if (mail($to, $subject, $messageBody, $headers)) {
-            $message = "<div class='form-message'>Message sent successfully!</div>";
-        } else {
-            $message = "<div>Error sending email.</div>";
-        }
-    } else {
-        $message = "<div>Please fill out all fields.</div>";
-    }
-}
-?>
-
 <section class='contact-me'>
-    <inner-column>
-        <contact-me>
-            <h2 class="loud-voice">Let's talk!</h2>
-            <p>Interested in working with me? Let's get in touch.</p>
+  <inner-column>
+    <contact-me>
+      <h2 class="loud-voice">Let's talk!</h2>
+      <p>Interested in working with me? Let's get in touch.</p>
 
-            <?=$message?>
+      <iframe
+        id="JotFormIFrame-251316628919161"
+        title="General Inquiry Contact Form"
+        onload="window.parent.scrollTo(0,0)"
+        allowtransparency="true"
+        allow="geolocation; microphone; camera; fullscreen"
+        src="https://form.jotform.com/251316628919161"
+        frameborder="0"
+        style="min-width:100%;max-width:100%;height:539px;border:none;"
+        scrolling="no"
+      ></iframe>
 
-            <form action="" method="post">
-                <input type="text" name="name" placeholder="Name"><br>
-                <input type="text" name="email" placeholder="Enter your email..."><br>
-                <textarea rows="5" name="message" cols="30" placeholder="Enter Message..."></textarea><br>
-                <input type="submit" name="submit" value="Submit" class="form-input">
-            </form>
-        </contact-me>
-    </inner-column>
+      <script src="https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js"></script>
+      <script>
+        window.jotformEmbedHandler("iframe[id='JotFormIFrame-251316628919161']", "https://form.jotform.com/");
+      </script>
+    </contact-me>
+  </inner-column>
 </section>
