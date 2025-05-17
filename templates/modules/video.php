238d440a70b69207cf0d6ad3a6381a079f371199
Joshua <joshuaegage@gmail.com>
2025-05-16 20:02:24 -0500

    video updates

--- templates/modules/video.php
+++ templates/modules/video.php
@@ -13,7 +13,9 @@
 
 <generic-text>
 
-<iframe width="560" height="315" src="<?=$video?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<div class="responsive-iframe">
+  <iframe src="<?=$video?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
 
 <?=$description?>
 
